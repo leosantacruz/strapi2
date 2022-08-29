0fde5d0b753619945176acf6db52ad909aef590a
Leonardo Santa Cruz <leo@MacBook-Pro-de-Leonardo.local>
2022-08-29 18:12:02 -0300

    Variables added

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'saldjalsjd23948u2893dskjnf'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', '38429839sdfkjshdfjsdhf'),
   },
 });
