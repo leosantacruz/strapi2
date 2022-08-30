bf2c09b7fd61d495b395aaeec25777fcee372e29
Leonardo Santa Cruz <leo@MacBook-Pro-de-Leonardo.local>
2022-08-30 20:17:53 -0300

    Server host

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'inspiring-cannoli-2d1295.netlify.app'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array("APP_KEYS", ["asjdhaksdjh2392834", "kdjsfhs23423kj42k34"]),
